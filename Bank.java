0c476f008ca5329f2125e9b31520aeced2b46c81
MANU779503 <manukc.xworkz@gmail.com>
2025-08-08 16:24:46 +0530

    java program

---
+++ Bank.java
@@ -0,0 +1,15 @@
+class Bank{
+public static void bucket(){
+System.out.println("this is a bucket");
+}
+
+public static void jug(){
+	  
+System.out.println("this is a jug");
+}
+public static void root(){
+System.out.println("this is a root");
+	
+}
+ 
+}
