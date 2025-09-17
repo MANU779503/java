8334c9b9a248d16659d2ce32005942e11d4b1de7
MANU779503 <manukc.xworkz@gmail.com>
2025-09-17 17:46:16 +0530

    for each

---
+++ DifferentClassAccessing/HotelRoom.java
@@ -0,0 +1,15 @@
+class HotelRoom {
+    public String guestName;
+    public int roomNumber;
+
+    public HotelRoom(String guestName, int roomNumber) {
+        this.guestName = guestName;
+        this.roomNumber = roomNumber;
+    }
+
+    public void hotelRoomDisplay() {
+        System.out.println(guestName);
+        System.out.println(roomNumber);
+    }
+}
+
